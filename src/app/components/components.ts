1881ab435b89bffbfb7c87fcf0271322cc32dbca
Mario Alberto dos Santos <mario.alberto.santos@avanade.com>
2017-08-19 12:13:14 -0300

    changes

--- src/app/components/components.ts
+++ src/app/components/components.ts
@@ -1,26 +1,22 @@
 import {DashboardsComponent} from './dashboards/dashboards.component';
 import {AppComponent} from '../app.component';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
-import {AuditoriaComponent} from './auditoria/auditoria.component';
 
 export {DashboardsComponent} from './dashboards/dashboards.component';
 export {AppComponent} from '../app.component';
 export {NavBarComponent} from './nav-bar/nav-bar.component';
-export {AuditoriaComponent} from './auditoria/auditoria.component';
 
 export default class Components {
   public static declarations = [
       DashboardsComponent,
       AppComponent,
-      NavBarComponent,
-      AuditoriaComponent
+      NavBarComponent
   ];
 
   public static exports = [
       DashboardsComponent,
       AppComponent,
-      NavBarComponent,
-      AuditoriaComponent
+      NavBarComponent
   ];
 
   public static bootstrap = [
