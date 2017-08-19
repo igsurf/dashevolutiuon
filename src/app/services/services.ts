345e5136e76c4196153508ba8451045cd3880f9c
Mario Alberto dos Santos <mario.alberto.santos@avanade.com>
2017-08-19 12:10:16 -0300

    cahnges

--- src/app/services/services.ts
+++ src/app/services/services.ts
@@ -1,5 +1,7 @@
 import {SocketService} from './socket.service';
 
+export {SocketService} from './socket.service';
+
 export default class Services {
     public static providers = [
         SocketService
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -8,132 +8,133 @@ import {Router} from '@angular/router';
 @Injectable()
 export class SocketService {
 
-  constructor(public router: Router) {
-  }
+    constructor(public router: Router) {
+    }
 
-  // private url = 'https://warm-wildwood-70824.herokuapp.com/chat';
-  private url = 'http://localhost:3000/dashboard';
+    // private url = 'https://warm-wildwood-70824.herokuapp.com/chat';
+    private url = 'http://localhost:3000/dashboard';
 
-  mensagens: Mensagem;
-  private socket;
+    mensagens: Mensagem;
+    private socket;
 
-  public user$: BehaviorSubject<String> = new BehaviorSubject(this.users);
+    public user$: BehaviorSubject<String> = new BehaviorSubject(this.users);
 
-  private _users;
-  public get users() {
-    return this._users;
-  }
+    private _users;
+    public get users() {
+        return this._users;
+    }
 
-  public set users(data) {
-    this._users = data;
-    this.user$.next(data);
-  }
+    public set users(data) {
+        this._users = data;
+        this.user$.next(data);
+    }
 
-  public messages$: BehaviorSubject<Mensagem> = new BehaviorSubject(this.messages);
+    public messages$: BehaviorSubject<Mensagem> = new BehaviorSubject(this.messages);
 
-  private _messages: Mensagem;
-  public get messages() {
-    return this._messages;
-  }
+    private _messages: Mensagem;
+    public get messages() {
+        return this._messages;
+    }
 
-  public set messages(data: Mensagem) {
-    this._messages = data;
-    this.messages$.next(data);
-  }
+    public set messages(data: Mensagem) {
+        this._messages = data;
+        this.messages$.next(data);
+    }
 
-  public dadosGraficoLinguagem$: BehaviorSubject<Object> = new BehaviorSubject(this.dadosGraficoLinguagens);
-  public dadosGraficoPaises$: BehaviorSubject<Object> = new BehaviorSubject(this.dadosGraficoPaises);
+    public dadosGraficoLinguagem$: BehaviorSubject<Object> = new BehaviorSubject(this.dadosGraficoLinguagens);
+    public dadosGraficoPaises$: BehaviorSubject<Object> = new BehaviorSubject(this.dadosGraficoPaises);
 
-  private _dadosGraficoLinguagem;
-  public get dadosGraficoLinguagens() {
-    return this._dadosGraficoLinguagem;
-  }
+    private _dadosGraficoLinguagem;
 
-  public set dadosGraficoLinguagens(data) {
-    this._dadosGraficoLinguagem = data;
-    this.dadosGraficoLinguagem$.next(data);
-  }
+    public get dadosGraficoLinguagens() {
+        return this._dadosGraficoLinguagem;
+    }
 
-  private _dadosGraficoPaises;
-  public get dadosGraficoPaises() {
-    return this._dadosGraficoPaises;
-  }
+    public set dadosGraficoLinguagens(data) {
+        this._dadosGraficoLinguagem = data;
+        this.dadosGraficoLinguagem$.next(data);
+    }
 
-  public set dadosGraficoPaises(data) {
-    this._dadosGraficoPaises = data;
-    this.dadosGraficoPaises$.next(data);
-  }
+    private _dadosGraficoPaises;
+    public get dadosGraficoPaises() {
+        return this._dadosGraficoPaises;
+    }
+
+    public set dadosGraficoPaises(data) {
+        this._dadosGraficoPaises = data;
+        this.dadosGraficoPaises$.next(data);
+    }
 
 
-  private subject = new Subject<any>();
+    private subject = new Subject<any>();
 
-  connect() {
-    this.socket = io(this.url);
+    connect() {
+        this.socket = io(this.url);
 
-    this.socket.on('connection', () => {
-      console.log('connection', 'Conectado ao websocket.');
-    });
+        this.socket.on('connection', () => {
+            console.log('connection', 'Conectado ao websocket.');
+        });
 
-    this.socket.on('user joined', (data) => {
-      this.users = data;
-    });
+        this.socket.on('user joined', (data) => {
+            this.users = data;
+        });
 
-    this.socket.on('new message', (data) => {
-      this.messages = data;
-    });
+        this.socket.on('new message', (data) => {
+            this.messages = data;
+        });
 
-    this.socket.on('go auditoria', () => {
-      this.router.navigate(['lista_auditoria'])
-    });
+        this.socket.on('go auditoria', () => {
+            this.router.navigate(['lista_auditoria'])
+        });
 
-    this.socket.on('users', (data) => {
-      var newObj = {
-        users: data.users
-      }
+        this.socket.on('users', (data) => {
+            var newObj = {
+                users: data.users
+            }
 
-      if (this.users) {
-        this.users.users = data.users;
-      } else {
-        this.users = newObj;
-      }
-    });
+            if (this.users) {
+                this.users.users = data.users;
+            } else {
+                this.users = newObj;
+            }
+        });
 
-    this.socket.on('carregarGraficoLinguagens', (dataLinguagens, dataPaises) => {
-      this.dadosGraficoLinguagens = dataLinguagens;
-      this.dadosGraficoPaises = dataPaises;
-    });
-  }
+        this.socket.on('carregarGraficoLinguagens', (dataLinguagens, dataPaises) => {
+            this.dadosGraficoLinguagens = dataLinguagens;
+            this.dadosGraficoPaises = dataPaises;
+        });
+    }
 
-  getMessages() {
-    const observable = new Observable<Mensagem>(observer => {
+    getMessages() {
+        const observable = new Observable<Mensagem>(observer => {
 
-    });
-    return observable;
-  }
+        });
+        return observable;
+    }
 
-  getDadosGraficoMensagem() {
-    const observable = new Observable<Object>(observer => {
+    getDadosGraficoMensagem() {
+        const observable = new Observable<Object>(observer => {
 
-    });
-    return observable;
-  }
+        });
+        return observable;
+    }
 
-  onUserJoined() {
-    const observable = new Observable<Mensagem>(observer => {
-    });
-    return observable;
-  }
+    onUserJoined() {
+        const observable = new Observable<Mensagem>(observer => {
+        });
+        return observable;
+    }
 
-  sendMessage(message) {
-    this.socket.emit('new message', message);
-    console.log("msg enviada");
-  }
+    sendMessage(message) {
+        this.socket.emit('new message', message);
+        console.log("msg enviada");
+    }
 
-  userLogin(userName: string) {
-    this.socket.emit('add user', userName);
-  }
+    userLogin(userName: string) {
+        this.socket.emit('add user', userName);
+    }
 
-  endStream() {
-    this.socket.emit('end stream');
-  }
+    endStream() {
+        this.socket.emit('end stream');
+    }
 }
